2d91284ba0bcd340f6899a6e23dedcadee53b1c4
viresh18 <viresh.renukoot@gmail.com>
2020-01-06 12:18:24 +0530

    Basic Template Structure

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World"
+    # return "Hello, World"
+    user = {'username': 'virekuma'}
+    # return """
+    # <html>
+    #     <head>
+    #         <title>Microblog</title>
+    #     </head>
+    #     <body>
+    #         <h1>Hi, """ + user['username'] + """</h1>
+    #     </body>
+    # </html>
+    # """
+    return render_template('index.html', title='Home', user=user)
