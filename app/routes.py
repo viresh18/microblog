6cf7839974fbb96d841eaa6765b5700b112f6b66
viresh18 <viresh.renukoot@gmail.com>
2020-01-06 13:11:56 +0530

    Conditional Statement.

--- app/routes.py
+++ app/routes.py
@@ -5,16 +5,5 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    # return "Hello, World"
     user = {'username': 'virekuma'}
-    # return """
-    # <html>
-    #     <head>
-    #         <title>Microblog</title>
-    #     </head>
-    #     <body>
-    #         <h1>Hi, """ + user['username'] + """</h1>
-    #     </body>
-    # </html>
-    # """
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', user=user)
